f66454a263cb30c067249193dd87996db3df43be
Des Flaherty <desflaherty@hotmail.com>
2019-02-16 15:28:57 +0000

    added second rowChart & labels

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,30 +8,46 @@ queue()
 //data will be passed into variable fireData by queue.min.js  
 //creating crossfilter
 
+
+
     function makeGraphs(error, fireData) {
     var ndx =crossfilter(fireData);
-     
-     /*   
+    
+    
     var parseDate = d3.time.format("%d/%m/%y").parse;
-       
         fireData.forEach(function(d){
-        d.date = parseDate(d.date);
+        d.Date = parseDate(d.Date);
+        });  
         
-       });
-        */
+     
+        
+        var countChart = dc.dataCount("#total_incidents");
+         countChart
+        .dimension(ndx)
+        .group(ndx.groupAll());
+       
+        
+           
         
         
        show_fire_by_area(ndx);
-       // show_fire_by_date(ndx);
+        show_fire_by_date(ndx);
         show_area_selector(ndx);
         show_year_selector(ndx);
         show_type_selector(ndx);
         show_fire_by_description(ndx);
+        show_fire_by_area10(ndx);
+       
         
         
         dc.renderAll(); //call to render dimensional charting
         
     }
+   
+  
+   
+   
+   
    
    function show_area_selector(ndx){
     var dim = ndx.dimension(dc.pluck('Station Area'));
@@ -61,29 +77,8 @@ queue()
    }
    
    
-   /*
-   function show_fire_by_date(ndx) {
-       var date_dim = ndx.dimension(dc.pluck('Date'));
-        var total_incidents_per_month = date_dim.group().reduce(dc.pluck('Description'));
-        var minDate = date_dim.bottom(1)[0].date;
-        var maxDate = date_dim.top(1)[0].date;
-        
-             dc.lineChart("#Fire-by-date")
-            .width(1000)
-            .height(300)
-            .margins({top: 10, right: 50, bottom: 30, left: 50})
-            .dimension(date_dim)
-            .group(total_incidents_per_month)
-            .transitionDuration(500)
-            .x(d3.time.scale().domain([minDate,maxDate]))
-            .xAxisLabel("Month")
-            .yAxis().ticks(4);
-       
-   }
    
-   */
-   
-
+  
 
     function show_fire_by_area(ndx) {
     var dim = ndx.dimension(dc.pluck('Station Area'));
@@ -108,14 +103,49 @@ function show_fire_by_description(ndx) {
     var group = dim.group();
     
          dc.rowChart("#Fire-by-description")
-        .width(1100)
+        .width(550)
         .height(300)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(dim)
         .group(group)
         .transitionDuration(500)//how quickly chart animates when filtered
-        .cap(10)
-       .othersGrouper(false);
-       
-      
+        .cap(10) //limit to top 10 records
+       .othersGrouper(false) // exclude grouped 'others'
 }
+
+function show_fire_by_area10(ndx) {
+    var dim = ndx.dimension(dc.pluck('Station Area'));
+    var group = dim.group();
+    
+         dc.rowChart("#Fire-by-area10")
+        .width(550)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)//how quickly chart animates when filtered
+        .cap(10) //limit to top 10 records
+       .othersGrouper(false) // exclude grouped 'others'
+}
+
+
+ function show_fire_by_date(ndx) {
+       var date_dim = ndx.dimension(dc.pluck('Date'));
+        var total_incidents_per_month = date_dim.group().reduceSum(dc.pluck('Description'));
+        var minDate = date_dim.bottom(1)[0].date;
+        var maxDate = date_dim.top(1)[0].date;
+        
+             dc.lineChart("#Fire-by-date")
+            .width(1000)
+            .height(300)
+            .margins({top: 10, right: 50, bottom: 30, left: 50})
+            .dimension(date_dim)
+            .group(total_incidents_per_month)
+            .transitionDuration(500)
+            .x(d3.time.scale().domain([minDate,maxDate]))
+            .xAxisLabel("Month")
+            .yAxis().ticks(4);
+       
+   }
+   
+   
